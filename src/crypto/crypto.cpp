132e56fa7adfe0d0f4b42797fa208b9e70df381b
David Fegyver <fegyverdavid.bator@gmail.com>
2025-02-08 10:25:08 +0100

    Sha256 on buffer

--- src/crypto/crypto.cpp
+++ src/crypto/crypto.cpp
@@ -9,7 +9,6 @@ std::vector<std::vector<uint8_t>> Crypto::split_vector(std::vector<uint8_t> data
     for (int i = 0; i < data.size(); i++)
     {
         result[i % n].push_back(data[i]);
-    
     }
     int max_len = result[0].size();
 
@@ -40,25 +39,52 @@ std::vector<uint8_t> Crypto::sha256(std::vector<uint8_t> data)
     return hash;
 }
 
+std::vector<uint8_t> Crypto::sha256(uint8_t *data, size_t data_len)
+{
+    mbedtls_sha256_context ctx;
+    mbedtls_sha256_init(&ctx);
+    mbedtls_sha256_starts(&ctx, 0);
+
+    mbedtls_sha256_update(&ctx, data, data_len);
+
+    std::vector<uint8_t> hash(32);
+    mbedtls_sha256_finish(&ctx, hash.data());
+
+    mbedtls_sha256_free(&ctx);
+
+    return hash;
+}
+
 int Crypto::calculate_n(int input_len, int alpha, int crypto_hash_len)
 {
     int n = ceil(sqrt(input_len * alpha / crypto_hash_len));
 
-
     int remainder = n % 6;
 
-    if (remainder == 0) {
+    if (remainder == 0)
+    {
         return n + 1;
     }
 
-    if (remainder == 1 || remainder == 5) {
+    if (remainder == 1 || remainder == 5)
+    {
         return n;
     }
-    
-    if (remainder == 2 || remainder == 3 || remainder == 4) {
-        return n + (5 - remainder); 
-    } 
-    
+
+    if (remainder == 2 || remainder == 3 || remainder == 4)
+    {
+        return n + (5 - remainder);
+    }
+
     return n;
 }
 
+String Crypto::uint8_vector_to_string(std::vector<uint8_t> data)
+{
+    String result = "";
+    for (int i = 0; i < data.size(); i++)
+    {
+        result += String(data[i], HEX);
+    }
+    return result;
+}
