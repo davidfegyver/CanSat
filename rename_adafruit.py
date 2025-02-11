eab34007e37245b0d272c4a05253e61b8097323e
David Fegyver <fegyverdavid.bator@gmail.com>
2025-02-11 15:23:57 +0100

    Fix for #11, replacing only whole-word occurences

--- rename_adafruit.py
+++ rename_adafruit.py
@@ -1,17 +1,18 @@
 import os 
-
-# replace every occurence of 'sensor_t' with 'adafruit_sensor_t' in .pio\libdeps\esp32cam
+import re
 
 path = '.pio\libdeps\esp32cam'
 
-print('Renaming sensor_t to adafruit_sensor_t in libraries')
+print('Renaming sensor_t to adafruit_sensor_t')
 
 for root, dirs, files in os.walk(path):
     for file in files:
         if file.endswith('.h') or file.endswith('.cpp'):
             with open(os.path.join(root, file), 'r') as f:
                 content = f.read()
-            content = content.replace('sensor_t', 'adafruit_sensor_t')
+
+            content = re.sub(r'\bsensor_t\b', 'adafruit_sensor_t', content)
+
             with open(os.path.join(root, file), 'w') as f:
                 f.write(content)
 
