2e07a5dde3d43354940da9cedc8ef99b493626df
David Fegyver <fegyverdavid.bator@gmail.com>
2025-02-11 15:31:07 +0100

    connectSdCard to camera

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ void initPeripherals()
     {
         SystemLog.connectSdCard();
         SensorLog.connectSdCard();
+        SystemCamera.connectSdCard();
     }
     SystemCamera.init();
     network.init();
